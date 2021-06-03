0bdaa5af23059902faa0d1fdfd13abb0a3c09270
Feryadi Aloi <feryadialoi@gmail.com>
2021-06-04 03:39:54 +0700

    add header to UserBalanceApiService.kt

--- app/src/main/java/dev/feryadi/bayu/apiservice/UserBalanceApiService.kt
+++ app/src/main/java/dev/feryadi/bayu/apiservice/UserBalanceApiService.kt
@@ -1,14 +1,19 @@
 package dev.feryadi.bayu.apiservice
 
+import dev.feryadi.bayu.constant.HttpHeader
+import dev.feryadi.bayu.constant.MediaType
 import dev.feryadi.bayu.model.network.response.ApiResponse
 import dev.feryadi.bayu.model.network.response.BalanceResponse
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Headers
 import retrofit2.http.Path
 
 interface UserBalanceApiService {
     @GET("/api/v1/users/{userId}/balances")
+    @Headers(value = [HttpHeader.ACCEPT + ":" + MediaType.APPLICATION_JSON])
     suspend fun getUserBalance(
-        @Header("Authorization") token: String,
-        @Path("userId") userId: Long): ApiResponse<BalanceResponse>
+        @Header(HttpHeader.AUTHORIZATION) token: String,
+        @Path("userId") userId: Long
+    ): ApiResponse<BalanceResponse>
 }
