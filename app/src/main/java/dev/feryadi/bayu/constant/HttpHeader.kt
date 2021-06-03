74dfd7f8de192795fb4c7a48519990eedafb4785
Feryadi Aloi <feryadialoi@gmail.com>
2021-06-04 03:38:00 +0700

    add HttpHeader.kt and MediaType.kt for constant value of api request

---
+++ app/src/main/java/dev/feryadi/bayu/constant/HttpHeader.kt
@@ -0,0 +1,7 @@
+package dev.feryadi.bayu.constant
+
+object HttpHeader {
+    const val AUTHORIZATION = "Authorization"
+    const val ACCEPT = "Accept"
+    const val CONTENT_TYPE = "Content-Type"
+}
---
+++ app/src/main/java/dev/feryadi/bayu/constant/MediaType.kt
@@ -0,0 +1,5 @@
+package dev.feryadi.bayu.constant
+
+object MediaType {
+    const val APPLICATION_JSON = "application/json"
+}
